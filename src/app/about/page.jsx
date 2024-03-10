ff7d03dfa9f98170edbeb4541780c4f6044b0fc7
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-10 17:27:29 +0530

    about page done.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-purple-600">About</h1>
+      <p>
+        Welcome to our IMDb-inspired movie database website built with Next.js
+        and Tailwind CSS! Dive into the world of cinema with our extensive
+        collection of movies and their detailed information.
+      </p>
+      <p>
+        With our user-friendly interface, you can effortlessly browse through
+        thousands of movies, ranging from timeless classics to the latest
+        releases. Whether you're a film enthusiast or simply looking for
+        something to watch, our platform offers a seamless experience for
+        discovering and exploring films of all genres.
+      </p>
+      <p>
+        Each movie page provides comprehensive details, including plot
+        summaries, cast and crew information, ratings, reviews, and more.
+        Powered by Next.js, our website ensures fast loading times and smooth
+        navigation, allowing you to immerse yourself in the magic of movies
+        without any distractions. Start your cinematic journey today with our
+        IMDb clone and unlock a world of entertainment at your fingertips!
+      </p>
+    </div>
+  );
+};
+
+export default About;
