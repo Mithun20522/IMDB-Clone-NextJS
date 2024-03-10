161bd5d5e5a65ede36a7aff3d697ab913c1293f6
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-10 18:05:14 +0530

    api integration and data fetching done.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import NavbarItem from './NavbarItem'
 
 const Navbar = () => {
   return (
-    <div className='flex dark:bg-gray-600 bg-purple-200 p-4 lg:text-lg justify-center gap-6'>
+    <div className='flex dark:bg-gray-600 bg-purple-100 p-4 lg:text-lg justify-center gap-6'>
         <NavbarItem title='Trending' param='fetchTrending'/>
         <NavbarItem title='Top Rated' param='fetchTopRated'/>
     </div>
